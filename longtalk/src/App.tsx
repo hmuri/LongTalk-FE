30d3d259a299a3a14000823266e89cdc4a4e2626
hmuri <113418916+hmuri@users.noreply.github.com>
2023-09-07 11:39:39 +0900

    feat: 기본 라우팅 설정 및 Main 페이지 생성

--- longtalk/src/App.tsx
+++ longtalk/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import styled from "styled-components";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Main from "./page/Main";
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div``;
