9b46af6cdff40f10fab41373fa2fbb76425efcbc
hmuri <113418916+hmuri@users.noreply.github.com>
2023-11-11 02:00:50 +0900

    feat: 가이드라인 완성

--- longtalk/src/page/Guideline.tsx
+++ longtalk/src/page/Guideline.tsx
@@ -23,119 +23,52 @@ export default function Guideline() {
         <MenuContainer>
           <br />
           <ContentContainer>
-            <Title>텀블벅</Title>
+            <Title>관람 유의사항</Title>
+            <SubTitle>
+              상상 그 이상을 꿈꾸며, 영화 커뮤니티 '상상마당 시네마'
+            </SubTitle>
             <br />
             <div>
               <p>
-                영화제 ‘롱톡(LongTalk)’을 개최하는 텀블벅 프로젝트는 팀 르포가
-                제작하는 한 단편영화에서 시작되었습니다. 가벼운 주제의 짧은
-                영상들을 반복적으로 소비하는 행동을 통해, 보다 쉽게 사회의
-                본질적인 측면을 외면하고 있는 문제를 인식한 데서 시작된
-                것입니다. 단편영화들을 통해 긴 대화를 나눌 수 있는 기회를
-                만들고자 함이 이 프로젝트를 시작한 핵심적 목표이자 기획
-                의도입니다.
+                서울 홍대 부근에 위치한 상상마당 시네마는 독립예술영화의 메카로
+                불리며 오랜 시간 좋은 영화를 발굴해왔습니다. 상영관 리뉴얼 후
+                2022년 1월 재개관해 영화를 사랑하는 모든 이들이 자유롭게
+                소통하는 ‘커뮤니티 시네마’로 거듭나 관객들에게 더욱 가깝게
+                다가가고 있습니다. 상상마당 시네마는 독립영화계 활성화 지원을
+                이어감과 동시에 다양한 영화 프로그램을 선보이며 기발한 상상력을
+                담은 영화를 발굴해 나갈 예정입니다.
               </p>
               <br />
 
               <p>
-                본 영화제의 예매를 텀블벅 프로젝트를 통해 받고 있습니다. 이에
-                따라 아래 링크를 통해 섹션을 예매하고 굿즈를 선택하실 수
-                있습니다.
+                상상마당 시네마는 상상마당 지하 4층에 위치해 있습니다. 또한
+                다음과 같은 사항을 미리 확인하여 유의해 주십시오.
               </p>
-              <br />
-              <br />
-              <img src={TumblbugLogo} />
-              <br />
-              <br />
-              <br />
-              <a
-                href="https://tumblbug.com/longtalk?ref=%EA%B2%80%EC%83%89%2F%ED%82%A4%EC%9B%8C%EB%93%9C"
-                style={{ textDecoration: "none" }}
-              >
-                <TumblbugBtn>텀블벅 바로가기</TumblbugBtn>
-              </a>
             </div>
             <br />
             <br />
-            <br />
-            <br />
-            <Title>예매 안내</Title>
-            <br />
-            <div>
+            <GuideBox>
               <p>
-                본 영화제는 네 섹션으로 이루어져 있으며 양일에 걸쳐 진행됩니다.
+                <b>
+                  1. 상영관 내 음식물 반입 제한 (상영관 입장 시 음료만 반입
+                  가능합니다)
+                </b>
               </p>
-              <br />
               <p>
-                <b>[발단 에디션]</b>을 선택하실 경우, 섹션1을 관람하실 수 있으며
-                키링이나 뱃지 중 랜덤 1종을 증정합니다.
+                <b>2. 정시 상영 시작 & 상영 시작 15분 후 입장 제한</b>
+                <br />
+                <div style={{ marginLeft: "18px" }}>
+                  교환, 환불은 상영 20분전까지 영수증 지참해야하며, 이후에는
+                  환불이 불가합니다.
+                </div>
               </p>{" "}
               <p>
                 {" "}
-                <b>[위기 에디션]</b>을 선택하실 경우, 섹션2를 관람하실 수 있으며
-                키링이나 뱃지 중 랜덤 1종을 증정합니다.{" "}
+                <b>3. 상영 종료 후 상영관 내 점등 </b>
               </p>
-              <p>
-                <b>[절정 에디션]</b>을 선택하실 경우, 섹션3을 관람하실 수 있으며
-                키링이나 뱃지 중 랜덤 1종을 증정합니다.{" "}
-              </p>
-              <p>
-                <b>[결말 에디션]</b>을 선택하실 경우, 섹션4를 관람하실 수 있으며
-                키링이나 뱃지 중 랜덤 1종을 증정합니다.{" "}
-              </p>
-              <p>
-                <b>[리미티드 에디션]</b>을 선택하실 경우, 섹션 하나를 선택하여
-                관람하실 수 있으며 키링이나 뱃지 중 랜덤 1종, 프로그램북을
-                증정합니다.
-              </p>
-              <br />
-              <br />
-            </div>
-            <Title>굿즈</Title>
+            </GuideBox>
+
             <br />
-            <br />
-            <img src={Keyring} />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginTop: "10px",
-              }}
-            >
-              <ImageBox />
-              <BookImgBox />
-            </div>
-            <br />
-            <div>
-              <p>
-                <b>키링: </b>영화제 ‘롱톡(LongTalk)’의 모티프가 된 릴레이소설을
-                '퍼즐'로 하여 풀어낸 디자인입니다. 이야기는 발단과 전개, 위기와
-                절정, 그리고 마지막 결말까지 모여 완성되듯이 영화제
-                ‘롱톡(LongTalk)’도 네 섹션에 이르러 이야기가 합쳐지고 더욱
-                강력한 메시지가 됩니다. 네 조각의 퍼즐로 빗댄 섹션들이 합쳐져
-                있는 모양이 키링으로 탄생했습니다.
-              </p>{" "}
-              <br />
-              <p>
-                {" "}
-                <b>뱃지: </b>영화제 ‘롱톡(LongTalk)’의 로고를 담아낸 디자인이
-                들어간 뱃지입니다. 위의 키링과 같이 영화제 ‘롱톡(LongTalk)’의
-                정체성을 담고 있습니다.{" "}
-              </p>
-              <br />
-              <p>
-                <b>프로그램북: </b>영화제 ‘롱톡(LongTalk)’의 제작 과정기를 담은
-                프로그램북입니다. 영화제 ‘롱톡(LongTalk)’의 자세한
-                기획의도에서부터 어떤 과정으로 프로그램을 구성하고
-                진행되어왔는지 보여줍니다. 더불어, 어떤 의도와 영감으로부터
-                포스터, 모션 그래픽, 사이트에 이르는 많은 작업물들이
-                디자인되었는지 공개합니다. 마지막 페이지에 있는 후원자 명단에
-                펀딩해주신 소중한 분들의 성함(닉네임)을 기입할 예정입니다.{" "}
-              </p>
-              <br />
-              <br />
-            </div>
           </ContentContainer>
         </MenuContainer>
         <Footer />
@@ -175,8 +108,10 @@ const ContentContainer = styled.div`
   width: 70%;
   margin: auto;
   padding: 8% 0;
+  font-size: 20px;
   @media ${(props) => props.theme.mobile} {
     padding: 16% 0;
+    font-size: 16px;
   }
 `;
 
@@ -192,34 +127,24 @@ const Title = styled.div`
   }
 `;
 
-const TumblbugBtn = styled.div`
-  color: black;
-
-  display: flex;
-  width: 200px;
-  height: 40px;
-  padding: 9px 33px;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  flex-shrink: 0;
-  border-radius: 40px;
-  border: 1px solid var(--black, rgba(0, 0, 0, 0.98));
-  background: var(--apricot, #f1ab85);
+const GuideBox = styled.div`
+  margin-left: 20px;
+  font-size: 20px;
+  @media ${(props) => props.theme.mobile} {
+    margin-left: 0;
+    font-size: 16px;
+  }
 `;
 
-const ImageBox = styled.div`
-  display: flex;
-  width: 41%;
-  padding-top: 41%;
-  background-image: url(${Badge});
-  background-size: cover;
-`;
-
-const BookImgBox = styled.div`
-  display: flex;
-  width: 57%;
-  padding-top: 40%;
-  background-image: url(${Book});
-  background-size: cover;
+const SubTitle = styled.div`
+  margin-top: 30px;
+  color: var(--black, rgba(0, 0, 0, 0.98));
+  font-family: Pretendard Variable;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 42px;
+  @media ${(props) => props.theme.mobile} {
+    font-size: 20px;
+  }
 `;
