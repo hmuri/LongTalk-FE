e5f2c766d21b7592826f01b51363b3184765dd9e
hmuri <113418916+hmuri@users.noreply.github.com>
2023-11-15 07:28:51 +0900

    style: button 추가

--- longtalk/src/page/Section.tsx
+++ longtalk/src/page/Section.tsx
@@ -13,6 +13,31 @@ import {
 
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import P1 from "../assets/image/poster/p_1.png";
+import P2 from "../assets/image/poster/p_2.png";
+import P3 from "../assets/image/poster/p_3.png";
+import P4 from "../assets/image/poster/p_4.png";
+import P5 from "../assets/image/poster/p_5.png";
+import P6 from "../assets/image/poster/p_6.png";
+import P7 from "../assets/image/poster/p_7.png";
+import P8 from "../assets/image/poster/p_8.png";
+import P9 from "../assets/image/poster/p_9.png";
+import P10 from "../assets/image/poster/p_10.png";
+import P11 from "../assets/image/poster/p_11.png";
+import P12 from "../assets/image/poster/p_12.png";
+
+import S1 from "../assets/image/stealcut/s_1.png";
+import S2 from "../assets/image/stealcut/s_2.png";
+import S3 from "../assets/image/stealcut/s_3.png";
+import S4 from "../assets/image/stealcut/s_4.png";
+import S5 from "../assets/image/stealcut/s_5.png";
+import S6 from "../assets/image/stealcut/s_6.png";
+import S7 from "../assets/image/stealcut/s_7.png";
+import S8 from "../assets/image/stealcut/s_8.png";
+import S9 from "../assets/image/stealcut/s_9.png";
+import S10 from "../assets/image/stealcut/s_10.png";
+import S11 from "../assets/image/stealcut/s_11.png";
+import S12 from "../assets/image/stealcut/s_12.png";
 
 export default function Section() {
   const { category } = useParams();
@@ -37,6 +62,37 @@ export default function Section() {
     if (nextIndex >= categories.length) nextIndex = 0;
 
     navigate(`/section/${categories[nextIndex]}`);
+  };
+
+  const getImagePath = (imageName: string): string | undefined => {
+    const imageMap: Record<string, string> = {
+      P1: P1,
+      P2: P2,
+      P3: P3,
+      P4: P4,
+      P5: P5,
+      P6: P6,
+      P7: P7,
+      P8: P8,
+      P9: P9,
+      P10: P10,
+      P11: P11,
+      P12: P12,
+      S1: S1,
+      S2: S2,
+      S3: S3,
+      S4: S4,
+      S5: S5,
+      S6: S6,
+      S7: S7,
+      S8: S8,
+      S9: S9,
+      S10: S10,
+      S11: S11,
+      S12: S12,
+    };
+
+    return imageMap[imageName] || undefined;
   };
 
   return (
@@ -66,8 +122,8 @@ export default function Section() {
           </svg>
         </CircleContainer>
         <SectionDescription>{description}</SectionDescription>
-        <button onClick={() => changeCategory("left")}>왼쪽</button>
-        <button onClick={() => changeCategory("right")}>오른쪽</button>
+        <LftBtn onClick={() => changeCategory("left")}></LftBtn>
+        <RgtBtn onClick={() => changeCategory("right")}></RgtBtn>
       </SubHeader>
       <FilmContainer>
         {movies.map((movie, index) => (
@@ -79,11 +135,17 @@ export default function Section() {
                   <br />
                   {movie.englishTitle}
                 </FilmInfoBox>
-                <StealCutImg>{movie.stealImage}</StealCutImg>
+                <StealCutImg
+                  src={getImagePath(movie.stealImage)}
+                  alt={movie.englishTitle}
+                />
               </>
             ) : (
               <>
-                <StealCutImg>{movie.stealImage}</StealCutImg>
+                <StealCutImg
+                  src={getImagePath(movie.stealImage)}
+                  alt={movie.englishTitle}
+                />
                 <FilmInfoBox>
                   {movie.koreanTitle}
                   <br />
@@ -148,24 +210,45 @@ const CircleContainer = styled.div`
 const FilmContainer = styled.div`
   display: flex;
   width: 100%;
-  background-color: grey;
   flex-direction: column;
 `;
 const SubFilmContainer = styled.div`
   display: flex;
   width: 100%;
   height: 420px;
-  background-color: white;
+  background-color: #efeae1;
   border: solid 1px red;
+  overflow: hidden;
 `;
 
-const StealCutImg = styled.div`
+const StealCutImg = styled.img`
   display: flex;
   flex: 0.7;
   border-right: solid 1px black;
+  object-fit: contain;
 `;
 
 const FilmInfoBox = styled.div`
   display: flex;
   flex: 0.3;
+`;
+
+const LftBtn = styled.button`
+  position: absolute;
+  height: 3px;
+  width: 3px;
+  left: 15px;
+  bottom: 148px;
+  z-index: 10;
+  background-color: black;
+`;
+
+const RgtBtn = styled.button`
+  position: absolute;
+  height: 3px;
+  width: 3px;
+  right: 15px;
+  bottom: 148px;
+  z-index: 10;
+  background-color: black;
 `;
